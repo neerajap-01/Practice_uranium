c9f2fef1610a6ccb4b1e957af0dc10cf467fcbc6
Neeraj <neerajap.01@gmail.com>
2022-04-14 01:55:57 +0530

    Week 3 Day 3 Assignment-Author&Book_Schema

--- src/controllers/allController.js
+++ src/controllers/allController.js
@@ -16,8 +16,6 @@ const addAuthor = async function(req, res){
 const bookbyChetanBhagat = async function(req, res){
     const showAuthor = await authorModel.findOne({author_name: "Chetan Bhagat"});
     
-    // const getAuthorIdTemp = showAuthor.map(data => data.author_id);
-    // const getAuthorID = getAuthorIdTemp[0];
     const getAuthorID = showAuthor.author_id;
 
     const allBooks = await bookModel.find({ author_id: getAuthorID }).select({ bookName: 1, _id: 0 });
@@ -27,11 +25,11 @@ const bookbyChetanBhagat = async function(req, res){
 const findandUpdateTwoStates = async function(req, res){
     const findUpdate = await bookModel.findOneAndUpdate(
         { bookName: "Two states" }, //condition 
-        { $set: { price: 100 } },
-        { new: true }
+        { $set: { price: 100 } }, //updating the price to 100
+        { new: true } //so that the data fetch is recent and not the pervious one
     )
     const getAuthorID = findUpdate.author_id;
-    const getUpdatePrice = await bookModel.findOne({ author_id: getAuthorID, }).select({ price: 1, _id: 0 })
+    const getUpdatePrice = await bookModel.findOne({ bookName: "Two states", }).select({ price: 1, _id: 0 })
 
     const allBooks = await authorModel.findOne({ author_id: getAuthorID }).select({ author_name: 1, _id: 0 });
     res.send( { data: [allBooks , getUpdatePrice] } );
@@ -42,8 +40,9 @@ const findBooks = async function(req, res){
 
     let getAuthorNames=[]
     for(let i = 0; i < getBookPrice.length; i++){
-        const getAuthorID = getBookPrice[i].author_id;
-        getAuthorNames = await authorModel.findOne({ author_id: getAuthorID }).select({ author_name: 1, _id: 0 });
+        let getAuthorID = getBookPrice[i].author_id;
+        let temp = await authorModel.findOne({ author_id: getAuthorID }).select({ author_name: 1, _id: 0 });
+        getAuthorNames.push(temp)
     }
     
     res.send( { data: getAuthorNames } );
