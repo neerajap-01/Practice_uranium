14e827a78446423364ba4883e530404c9e450195
Neeraj <neerajap.01@gmail.com>
2022-04-19 20:41:21 +0530

    Week 4 Day 2 Assignment-Middleware_Intro

--- src/middleWare/middleWare.js
+++ src/middleWare/middleWare.js
@@ -2,10 +2,8 @@ const logData = function(req, res, next){
   let timeStamp = new Date();
   let date = timeStamp.getFullYear() + '-' + (timeStamp.getMonth() + 1) + '-' + timeStamp.getDate() + ' ' + timeStamp.getHours() + ':' + timeStamp.getMinutes() + ':' + timeStamp.getSeconds();
   
-
-  console.log(date);
-  console.log(req.ip);
-  console.log(req.path);
+  const result = date + ", " + req.ip + ", " + req.path;
+  console.log(result);
   next();
 };
 
